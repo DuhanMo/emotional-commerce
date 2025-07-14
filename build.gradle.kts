5df1f41dea232f4b7ce467cfab6415bcab9a4fee
Duhan Mo <duhandv@gmail.com>
2025-07-14 20:04:11 +0900

    chore: kotest gradle 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,6 +82,10 @@ subprojects {
         testImplementation("org.springframework.boot:spring-boot-testcontainers")
         testImplementation("org.testcontainers:testcontainers")
         testImplementation("org.testcontainers:junit-jupiter")
+        // kotest
+        testImplementation("io.kotest:kotest-runner-junit5:${project.properties["kotestVersion"]}")
+        testImplementation("io.kotest:kotest-assertions-core:${project.properties["kotestVersion"]}")
+        testImplementation("io.kotest:kotest-property:${project.properties["kotestVersion"]}")
     }
 
     tasks.withType(Jar::class) { enabled = true }
