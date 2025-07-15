d07c0eb2594bd2c4953b08f11dd9451bc9e1955f
Duhan Mo <duhandv@gmail.com>
2025-07-15 18:58:14 +0900

    style: 테스트 인덴트 수정

--- apps/commerce-api/src/test/kotlin/com/loopers/application/UserServiceIGTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/application/UserServiceIGTest.kt
@@ -24,48 +24,46 @@ class UserServiceIGTest @Autowired constructor(
     private val userJpaRepository: UserJpaRepository,
     private val databaseCleanUp: DatabaseCleanUp,
     @SpykBean private val userWriter: UserWriter,
-) : BehaviorSpec(
-    {
-        extensions(SpringExtension)
+) : BehaviorSpec({
+    extensions(SpringExtension)
 
-        afterTest {
-            databaseCleanUp.truncateAllTables()
-        }
+    afterTest {
+        databaseCleanUp.truncateAllTables()
+    }
 
-        Given("이미 가입된 ID 가 없는 경우") {
-            val command = createUserCreateCommand()
+    Given("이미 가입된 ID 가 없는 경우") {
+        val command = createUserCreateCommand()
 
-            When("회원 가입 하면") {
-                userService.register(command)
+        When("회원 가입 하면") {
+            userService.register(command)
 
-                Then("User 저장이 수행된다") {
-                    verify(exactly = 1) { userWriter.write(any()) }
-                }
+            Then("User 저장이 수행된다") {
+                verify(exactly = 1) { userWriter.write(any()) }
             }
         }
+    }
 
-        Given("이미 가입된 ID 가 있는 경우") {
-            val existUid = "test123"
-            userJpaRepository.save(
-                User(
-                    uid = existUid,
-                    email = "test@email.com",
-                    birthDate = "2020-01-01",
-                ),
-            )
-            val command = createUserCreateCommand(uid = existUid)
+    Given("이미 가입된 ID 가 있는 경우") {
+        val existUid = "test123"
+        userJpaRepository.save(
+            User(
+                uid = existUid,
+                email = "test@email.com",
+                birthDate = "2020-01-01",
+            ),
+        )
+        val command = createUserCreateCommand(uid = existUid)
 
-            When("회원 가입 하면") {
-                Then("예외 발생한다") {
-                    val exception = assertThrows<CoreException> {
-                        userService.register(command)
-                    }
-                    exception.message shouldBe "이미 존재하는 ID 입니다"
+        When("회원 가입 하면") {
+            Then("예외 발생한다") {
+                val exception = assertThrows<CoreException> {
+                    userService.register(command)
                 }
+                exception.message shouldBe "이미 존재하는 ID 입니다"
             }
         }
-    },
-)
+    }
+})
 
 private fun createUserCreateCommand(
     uid: String = "test123",
