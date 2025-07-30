2b3779fd966a849d48ef7855ab69b7531744a29d
Duhan Mo <duhandv@gmail.com>
2025-07-30 22:41:20 +0900

    test: 상품 좋아요 단위 테스트 수정

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/product/ProductLikeTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/product/ProductLikeTest.kt
@@ -7,14 +7,14 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
 class ProductLikeTest : StringSpec({
-    "좋아요 최초 생성하면 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
+    "싱픔 좋아요 최초 생성하면 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
         val productLike = ProductLike(productId = 1L, userId = 1L)
 
         productLike.status shouldBe ACTIVE
         productLike.deletedAt shouldBe null
     }
 
-    "좋아요를 취소하면 상태는 DELETED 이고 deletedAt 이 not null 이다" {
+    "상품 좋아요를 좋아요 취소하면 상태는 DELETED 이고 deletedAt 이 not null 이다" {
         val productLike = ProductLike(productId = 1L, userId = 1L)
 
         productLike.unlike()
@@ -23,7 +23,7 @@ class ProductLikeTest : StringSpec({
         productLike.deletedAt shouldNotBe null
     }
 
-    "취소된 좋아요를 다시 좋아요 등록하면 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
+    "취소된 상품 좋아요를 다시 좋아요 등록하면 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
         val productLike = ProductLike(productId = 1L, userId = 1L)
 
         productLike.unlike()
@@ -34,21 +34,19 @@ class ProductLikeTest : StringSpec({
         productLike.deletedAt shouldBe null
     }
 
-    "좋아요를 두 번 등록해도 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
+    "상품 좋아요를 좋아요 등록을 두 번해도 상태는 ACTIVE 이고 deletedAt 이 null 이다" {
         val productLike = ProductLike(productId = 1L, userId = 1L)
 
-        productLike.like()
-        productLike.like()
+        repeat(2) { productLike.like() }
 
         productLike.status shouldBe ACTIVE
         productLike.deletedAt shouldBe null
     }
 
-    "좋아요를 두 번 취소해도 상태는 DELETED 이고 deletedAt 이 not null 이다" {
+    "상품 좋아요를 좋아요 취소를 두 번 해도 상태는 DELETED 이고 deletedAt 이 not null 이다" {
         val productLike = ProductLike(productId = 1L, userId = 1L)
 
-        productLike.unlike()
-        productLike.unlike()
+        repeat(2) { productLike.unlike() }
 
         productLike.status shouldBe DELETED
         productLike.deletedAt shouldNotBe null
