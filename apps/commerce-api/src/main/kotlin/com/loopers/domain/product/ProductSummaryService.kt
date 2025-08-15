52aa8f8edeaee42b8db97611939f48018c7d140f
Duhan Mo <duhandv@gmail.com>
2025-08-15 15:40:30 +0900

    fix: 좋아요 집게 수 증가 동기화 적용

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/product/ProductSummaryService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/product/ProductSummaryService.kt
@@ -1,9 +1,5 @@
 package com.loopers.domain.product
 
-import org.springframework.dao.OptimisticLockingFailureException
-import org.springframework.retry.annotation.Backoff
-import org.springframework.retry.annotation.Retryable
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,13 +7,7 @@ import org.springframework.transaction.annotation.Transactional
 class ProductSummaryService(
     private val productSummaryRepository: ProductSummaryRepository,
 ) {
-    @Async
     @Transactional
-    @Retryable(
-        value = [OptimisticLockingFailureException::class],
-        maxAttempts = 5,
-        backoff = Backoff(delay = 50, multiplier = 2.0),
-    )
     fun increaseLikeCount(productId: Long) {
         val productSummary = productSummaryRepository.getByProductId(productId)
 
@@ -26,13 +16,7 @@ class ProductSummaryService(
         productSummaryRepository.save(productSummary)
     }
 
-    @Async
     @Transactional
-    @Retryable(
-        value = [OptimisticLockingFailureException::class],
-        maxAttempts = 5,
-        backoff = Backoff(delay = 50, multiplier = 2.0),
-    )
     fun decreaseLikeCount(productId: Long) {
         val productSummary = productSummaryRepository.getByProductId(productId)
 
